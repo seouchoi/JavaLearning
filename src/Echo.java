95f7bc7629aaec4779bbd47e231d8a2b9332aaba
tjdn1 <tjdn1@DESKTOP-7SR206A>
2023-03-17 21:57:12 +0900

    .

---
+++ src/Echo.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+public class Echo {
+
+	public static void main(String[] args) {
+		Scanner echo = new Scanner(System.in);
+		System.out.print("Write your comment : ");
+		String a =echo.nextLine();
+		System.out.println(a);
+	}
+
+}
---
+++ src/Temperture.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Temperture {
+
+	public static void main(String[] args) {
+		Scanner a= new Scanner(System.in);
+		System.out.print("Fahrenheit temperture : ");
+		double fatem=a.nextDouble();
+		double celtem= (double)5/9*(fatem-32);
+		System.out.println("Celsius temperture : "+celtem);
+		
+
+	}
+
+}
