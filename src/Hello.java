3e0b7e7489399b7fe09fe65251a25a3642b3a3f6
tjdn1 <tjdn1@DESKTOP-35CG8GB>
2023-03-21 17:18:06 +0900

    fda

--- src/Hello.java
+++ src/Hello.java
@@ -4,6 +4,7 @@ public class Hello {
 	public static void main(String[] args) {
 		System.out.println("Hello world This is notebook.");
 		System.out.println("Hi this is desktop");
+		System.out.println("Nice to meet you");
 
 	}
 
