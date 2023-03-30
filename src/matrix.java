9524a0cbf8047ac01983eb6ce0c2791284d3280a
tjdn1 <tjdn1@DESKTOP-35CG8GB>
2023-03-30 15:09:39 +0900

    matrix

---
+++ src/matrix.java
@@ -0,0 +1,25 @@
+
+public class matrix {
+
+	public static void main(String[] args) {
+		int matrixA[][]= {{1,0,2},{-1,3,1}};
+		int matrixB[][]= {{3,1},{2,1},{1,0}};
+		int matrixC[][] = new int[matrixA.length][matrixB[0].length];
+		
+		
+		for(int i=0;i<matrixA.length;i++)
+		{
+			for(int j=0;j<matrixB[0].length;j++)
+			{
+				for(int k=0;k<matrixA[0].length;k++)
+				{
+					matrixC[i][j]+=matrixA[i][k]*matrixB[k][j];
+				}
+			}
+		}
+		System.out.println(matrixC[0][0]+" "+matrixC[0][1]);
+		System.out.println(matrixC[1][0]+" "+matrixC[1][1]);
+
+	}
+
+}
