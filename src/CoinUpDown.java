0efcb8c369a08040245ce3ddf18be1f35886332a
서우 최 <tjdn1080@gmail.com>
2023-03-31 19:40:50 +0900

    HomeWork

---
+++ src/CoinUpDown.java
@@ -0,0 +1,32 @@
+import java.util.*;
+
+public class CoinUpDown {
+
+	static String Coin() {
+		int randomNum=(int)(Math.random()*2);
+		if(randomNum<1)
+		{
+			return "H";
+		}else if(randomNum>=1)
+		{
+			return "T";
+		}
+		return null;
+		
+	}
+	
+	public static void main(String[] args) {
+		Scanner input=new Scanner(System.in);
+		System.out.print("Choice H or T : ");
+		String Choice=input.nextLine();
+		
+		if(Choice.equals(Coin()))
+		{
+			System.out.println("Correct!");
+		}else {
+			System.out.println("Incorrect..");
+		}
+		
+	}
+
+}
